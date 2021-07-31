42b080bfece861647fc53892e7e0c97d68806117
SerjMartin <serjmartin@mail.com>
2021-07-31 12:53:26 +0000

    Creat get_sales_data function to get the data string rom user

--- run.py
+++ run.py
@@ -13,8 +13,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():# this function colect sale data from our users
+     """
+     Get sales figures input from the users
+     """
+     print("Please enter sales data from the last market.")
+     print("Data should be six numbers, separted by commas.")
+     print("Exemple: 10, 20, 30, 40, 50, 60\n") # using \n for space betwen new line
 
-data = sales.get_all_values()
+     data_str = input("Enter your data here:")# use input() method to get our sale data from the users to the terminal
+     print(f"The data provide is {data_str}")#will print the data_str provided back to the terminal
 
-print(data)
+get_sales_data()
