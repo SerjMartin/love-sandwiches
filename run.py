8f158a4ff490d6a6c2d788afb1bea72af0df0861
SerjMartin <serjmartin@mail.com>
2021-08-01 15:18:13 +0000

    Refactor update worksheet function into one

--- run.py
+++ run.py
@@ -83,10 +83,12 @@ def validate_data(values):
     return True
 
 
+"""
+
 def update_sales_worksheet(data):
-    """
-     Update sales worksheet, add new row with the list data provided.
-    """
+    ----------
+     (Update sales worksheet, add new row with the list data provided.)
+    ----------
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
@@ -94,13 +96,26 @@ def update_sales_worksheet(data):
 
 
 def update_surplus_worksheet(data):
-    """
-     Update surplus worksheet, add new row with the list data provided.
-    """
+    ---------
+     (Update surplus worksheet, add new row with the list data provided.)
+    ---------
     print("Updating surplus worksheet...\n")
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfuly.\n")
+"""
+
+
+def update_worksheet(data, worksheet):
+    """
+     Receives a list of integers to be inserted into worksheet
+     Update the relevant worksheet with the data providet
+     (This function will replace those two function on top)
+     calls Refactoring
+    """
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -142,9 +157,9 @@ def main():
     """
      used int() method to convert num data to integer
     """
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
     """
      Call out the function (update_sales_worksheet(data))
      and pas it sales_data list
